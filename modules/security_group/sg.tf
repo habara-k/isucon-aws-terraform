968e4fb1f52c88dd1c39dad969d73a5a65e321a0
Keigo Habara <k.habara.aa@gmail.com>
2026-08-08 12:59:44 +0900

    Allow all ingress port

--- modules/security_group/sg.tf
+++ modules/security_group/sg.tf
@@ -3,57 +3,12 @@ resource "aws_security_group" "default" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_security_group_rule" "ingress_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = var.cidr_blocks
-  security_group_id = aws_security_group.default.id
-}
-
-resource "aws_security_group_rule" "ingress_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = var.cidr_blocks
-  security_group_id = aws_security_group.default.id
-}
-
-resource "aws_security_group_rule" "ingress_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = var.cidr_blocks
-  security_group_id = aws_security_group.default.id
-}
-
-resource "aws_security_group_rule" "ingress_mysql" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = var.cidr_blocks
-  security_group_id = aws_security_group.default.id
-}
-
-resource "aws_security_group_rule" "ingress_netdata" {
-  type              = "ingress"
-  from_port         = 19999
-  to_port           = 19999
-  protocol          = "tcp"
-  cidr_blocks       = var.cidr_blocks
-  security_group_id = aws_security_group.default.id
-}
-
-resource "aws_security_group_rule" "ingress_self" {
+resource "aws_security_group_rule" "ingress_all" {
   type              = "ingress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  self              = true
+  cidr_blocks       = var.cidr_blocks
   security_group_id = aws_security_group.default.id
 }
 
